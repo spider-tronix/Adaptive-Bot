7a6401811b59f76e05909a225d4c6728cd9b2cb2
sachin-101 <sachinkumar04428@gmail.com>
2020-03-10 11:02:33 +0530

    Modified scene included dummy object to detect collision

--- Software/vrep_tests/Poppy_Ergo/env.py
+++ Software/vrep_tests/Poppy_Ergo/env.py
@@ -7,16 +7,20 @@ import sim
 import time
 from math import sin, cos, radians, pi
 
+# RemoteAPI constants
+streaming = sim.simx_opmode_streaming
+blocking = sim.simx_opmode_blocking
+buffer = sim.simx_opmode_buffer
+oneshot = sim.simx_opmode_oneshot
+
 ANGLE_DISCRETIZATION = 10
 BOX_DISTANCE = 0.2
 
 class PoppyEnv(gym.Env):
     
-    def __init__(self, clientID, faulty_joints, green_box, red_box):
+    def __init__(self, clientID, faulty_joints):
         self.num_joints = 6
         self.clientID = clientID
-        self.green_box = green_box
-        self.red_box = red_box
         if faulty_joints != -1:
             self.faulty_joints = [int(joint) for joint in faulty_joints]
         else:
@@ -24,53 +28,58 @@ class PoppyEnv(gym.Env):
         self.action_map = {0:0, 1:ANGLE_DISCRETIZATION, 2:-ANGLE_DISCRETIZATION}
         
         self.action_size = self.num_joints*3        # 0, left, right
-        self.state_size = self.num_joints + 3       # 6 DOF + (x, y, z) box cords
+        self.state_size = self.num_joints + 3       # 6 DOF + (x, y, z) box distance cords
 
         self._setup_spaces()
         self._get_joint_handles()
         self._init_pos()
 
     def _get_joint_handles(self):
-        self.joint_handles = []
+        self.green_box = sim.simxGetObjectHandle(self.clientID, "Green_Box", blocking)[1]
+        self.red_box = sim.simxGetObjectHandle(self.clientID, "Red_Box", blocking)[1]  
+        self.lamp_end = sim.simxGetObjectHandle(self.clientID, "lamp_end", blocking)[1]  
+        
+        self.joint_handles = [] # get joint handles
         for i in range(1, self.num_joints+1):
-            _, handle = sim.simxGetObjectHandle(self.clientID, f"m{i}", sim.simx_opmode_blocking)
+            _, handle = sim.simxGetObjectHandle(self.clientID, f"m{i}", blocking)
             self.joint_handles.append(handle)
         
-
     def _init_pos(self):
         # print positions
         for i, handle in enumerate(self.joint_handles, start=1):
-            print(f"m{i}_pos", sim.simxGetJointPosition(self.clientID, handle, sim.simx_opmode_streaming)[1])
+            print(f"m{i}_pos", sim.simxGetJointPosition(self.clientID, handle, streaming)[1])
         print(f"Faulty joints {self.faulty_joints}")
+        print("Distance from Green Box: " ,sim.simxGetObjectPosition(self.clientID, self.lamp_end, self.green_box, streaming)[1])
 
     def _setup_spaces(self):
         self.action_space = spaces.Discrete(self.action_size)
-        self.observation_space = spaces.Box(low=np.array([-5.0, -5.0, -2.62, -2.62, -2.62, -2.62, -2.62, -2.62]), 
-                                            high=np.array([5.0, 5.0, 2.62, 2.62, 2.62, 2.62, 2.62, 2.62]),
-                                            dtype=np.float32)
+        # self.observation_space = spaces.Box(low=np.array([-5.0, -5.0, -2.62, -2.62, -2.62, -2.62, -2.62, -2.62]), 
+        #                                     high=np.array([5.0, 5.0, 2.62, 2.62, 2.62, 2.62, 2.62, 2.62]),
+        #                                     dtype=np.float32)
 
     def step(self, action):
-            assert self.action_space.contains(action)
-            
-            joint = int(action/3)
-            handle = self.joint_handles[joint]
-            value = self.action_map[action%3]
-            
-            if joint in self.faulty_joints:
-                value = 0 
+        assert self.action_space.contains(action)
+        
+        joint = int(action/3)
+        handle = self.joint_handles[joint]
+        value = self.action_map[action%3]
+        
+        if joint in self.faulty_joints:
+            value = 0 
 
-            #for angle, handle in zip(action, self.joint_handles)        
-            cur_pos = sim.simxGetJointPosition(self.clientID,handle,sim.simx_opmode_buffer)[1]
-            sim.simxSetJointTargetPosition(self.clientID,handle,cur_pos+value*(3.14/180),sim.simx_opmode_oneshot)
+        #for angle, handle in zip(action, self.joint_handles)        
+        cur_pos = sim.simxGetJointPosition(self.clientID,handle,buffer)[1]
+        sim.simxSetJointTargetPosition(self.clientID,handle,cur_pos+value*(3.14/180),oneshot)
 
-            # give reward for make it straight
-            reward, done = self.detect_collision()
+        # give reward for touching the box
+        reward, done = self.detect_collision()
 
-            state = [self.box_x, self.box_y, self.box_z]
-            for handle in self.joint_handles:
-                state.append(sim.simxGetJointPosition(self.clientID, handle, sim.simx_opmode_buffer)[1])
+        box_distance = sim.simxGetObjectPosition(self.clientID, self.lamp_end, self.green_box, buffer)[1]
+        state = [d for d in box_distance]
+        for handle in self.joint_handles:
+            state.append(sim.simxGetJointPosition(self.clientID, handle, buffer)[1])
 
-            return np.array(state), reward, done, {}
+        return np.array(state), reward, done, {}
     
     def detect_collision(self):
         reward = 0
@@ -81,7 +90,7 @@ class PoppyEnv(gym.Env):
                                                             "remoteApiCommandServer",
                                                             sim.sim_scripttype_childscript,
                                                             "collision_with_box",[],[],[],
-                                                            emptyBuff,sim.simx_opmode_blocking)
+                                                            emptyBuff,blocking)
 
         if result == sim.simx_return_ok:
             if collision_state[0] == 1:  # Collided with green box
@@ -97,22 +106,19 @@ class PoppyEnv(gym.Env):
         
   
   
-    def reset(self):
-        for handle in self.joint_handles:
-            sim.simxSetJointTargetPosition(self.clientID, handle, 0, 
-                                            sim.simx_opmode_oneshot)
+    def reset(self):        
+        for handle in self .joint_handles:
+            sim.simxSetJointTargetPosition(self.clientID, handle, 0, blocking)
         # Change Box positions
-        rand_angle = radians(random.randint(0, 359))        
-        self.box_x, self.box_y = (BOX_DISTANCE*cos(rand_angle), BOX_DISTANCE*sin(rand_angle))
-        self.box_z = random.randint(0,2)*0.1
-        sim.simxSetObjectPosition(self.clientID, self.green_box, self.joint_handles[0], 
-                                    (self.box_x, self.box_y, self.box_z), sim.simx_opmode_oneshot)
-        sim.simxSetObjectPosition(self.clientID, self.red_box, self.joint_handles[0], 
-                                    (-self.box_x, -self.box_y, self.box_z), sim.simx_opmode_oneshot)
+        rand_angle = 0 # radians(random.randint(0, 359))        
+        box_x, box_y = (BOX_DISTANCE*cos(rand_angle), BOX_DISTANCE*sin(rand_angle))
+        sim.simxSetObjectPosition(self.clientID, self.green_box, self.joint_handles[0], (box_x, box_y, 0), oneshot)
+        sim.simxSetObjectPosition(self.clientID, self.red_box, self.joint_handles[0], (-box_x, -box_y, 0), oneshot)
         
-        state = [self.box_x, self.box_y, self.box_z]
+        box_distance = sim.simxGetObjectPosition(self.clientID, self.lamp_end, self.green_box, buffer)[1]
+        state = [d for d in box_distance]
         for handle in self.joint_handles:
-            state.append(sim.simxGetJointPosition(self.clientID, handle, sim.simx_opmode_buffer)[1])
+            state.append(sim.simxGetJointPosition(self.clientID, handle, buffer)[1])
         return np.array(state)
 
     
