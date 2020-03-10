78ec29d8b769cd913d730e4841e8356732521059
sachin-101 <sachinkumar04428@gmail.com>
2020-03-10 17:49:55 +0530

    Hexapod ready to train
    But since the training is in asynchronous mode, it is very slow

--- Software/vrep_tests/Hexapod/hexapod_env.py
+++ Software/vrep_tests/Hexapod/hexapod_env.py
@@ -14,22 +14,23 @@ blocking = sim.simx_opmode_blocking
 buffer = sim.simx_opmode_buffer
 oneshot = sim.simx_opmode_oneshot
 
-ANGLE_DISCRETIZATION = 10
+ANGLE_DISCRETIZATION = 30
 GOAL_DISTANCE = 0.2
 DISTANCE_TO_WALK = 0.5
+MAX_ANGLE = 30*(3.14/180)
 
 class HexapodEnv(gym.Env):
     
     def __init__(self, clientID, faulty_joints, test=True):
-        self.num_joints = 12
+        self.num_joints = 6
         self.clientID = clientID
         if faulty_joints != -1:
             self.faulty_joints = [int(joint) for joint in faulty_joints]
         else:
             self.faulty_joints = []
-        self.action_map = {0:0, 1:ANGLE_DISCRETIZATION, 2:-ANGLE_DISCRETIZATION}
+        self.action_map = {0:ANGLE_DISCRETIZATION, 1:-ANGLE_DISCRETIZATION}
         
-        self.action_size = self.num_joints*3        # 0, left, right
+        self.action_size = self.num_joints*2        # left, right
         self.state_size = self.num_joints   # Current Pos of each servo
 
         self.test = test
@@ -37,14 +38,27 @@ class HexapodEnv(gym.Env):
         self._get_joint_handles()
         self._init_pos()
 
+
+
     def _get_joint_handles(self):
         self.hexapod_handle = sim.simxGetObjectHandle(self.clientID, 'hexapod', blocking)[1] 
         print("Hexapod handle", self.hexapod_handle)       
         self.joint_handles = {} # get joint handles
         for i in range(6):
-            for j in range(2):
+            for j in range(1):
                 joint = f"hexa_joint{j+1}_{i}"
                 self.joint_handles[joint] =  sim.simxGetObjectHandle(self.clientID, joint, blocking)[1]
+
+        # list of 3 dicts
+        self.links = []  # organise them according to link positions
+        for j in range(3):
+            handles = {}
+            for i in range(6):
+                joint = f"hexa_joint{j+1}_{i}"
+                handles[joint] =  sim.simxGetObjectHandle(self.clientID, joint, blocking)[1]
+            self.links.append(handles)
+        
+
 
     def _init_pos(self):
         # Set to streaming mode
@@ -70,11 +84,13 @@ class HexapodEnv(gym.Env):
         print(f"Faulty joints {self.faulty_joints}")
 
         
+
     def _setup_spaces(self):
         self.action_space = spaces.Discrete(self.action_size)
         # self.observation_space = spaces.Box(low=np.array([-5.0, -5.0, -2.62, -2.62, -2.62, -2.62, -2.62, -2.62]), 
         #                                     high=np.array([5.0, 5.0, 2.62, 2.62, 2.62, 2.62, 2.62, 2.62]),
         #                                     dtype=np.float32)
+
 
     def reset(self):        
         
@@ -86,8 +102,10 @@ class HexapodEnv(gym.Env):
                                                             "reset_robot",[],[],[],
                                                             emptyBuff,blocking)
 
-        for handle in self.joint_handles.values():
-            sim.simxSetJointTargetPosition(self.clientID, handle, 0, blocking)
+        for link, angle in zip(self.links, [0, -30, 120]):
+            for handle in link.values():
+                sim.simxSetJointTargetPosition(self.clientID, handle, angle*(3.14/180), blocking)
+        
         self.prev_pos = sim.simxGetObjectPosition(self.clientID, self.hexapod_handle, -1, buffer)[1]
         return np.zeros(self.state_size)     # hope coppeliaSim set them properly
 
@@ -95,20 +113,33 @@ class HexapodEnv(gym.Env):
     def step(self, action):
         assert self.action_space.contains(action)
         
-        joint = int(action/3)
+        joint = int(action/2)
         handle = list(self.joint_handles.values())[joint]
-        value = self.action_map[action%3]
+        parent_handle = list(self.links[1].values())[joint]
+        value = self.action_map[action%2]
         
         if joint in self.faulty_joints:
             value = 0 
         cur_pos = sim.simxGetJointPosition(self.clientID,handle,buffer)[1]
-        sim.simxSetJointTargetPosition(self.clientID,handle,cur_pos+value*(3.14/180),oneshot)
-
-        reward, done = self.calc_reward()
+        new_pos = cur_pos + value*(3.14/180)
+        if np.abs(new_pos) <= MAX_ANGLE:   # Limiting it from rotating beyond certain degree
+            sim.simxSetJointTargetPosition(self.clientID,parent_handle,-45*(3.14/180),blocking)
+            sim.simxSetJointTargetPosition(self.clientID,handle,new_pos,oneshot)
+            sim.simxSetJointTargetPosition(self.clientID,parent_handle,-30*(3.14/180),blocking)
+            reward, done = self.calc_reward()
+        else:
+            reward = -1
+            done = False
+        
         state = []
+        # CAN FASTEn this by including only the one that is changed
         for handle in self.joint_handles.values():
             state.append(sim.simxGetJointPosition(self.clientID, handle, buffer)[1])
 
+        # straighten the final positions
+        # for handle in self.links[2].values():
+        #     sim.simxSetJointTargetPosition(self.clientID,handle,120*(3.14/180),oneshot)
+            
         return np.array(state), reward, done, {}
     
 
@@ -118,8 +149,10 @@ class HexapodEnv(gym.Env):
         cur_pos = sim.simxGetObjectPosition(self.clientID, self.hexapod_handle, -1, buffer)[1]
 
         if cur_pos[2] - self.prev_pos[2] > 0.001:
+            print("m")
             reward = 1
         elif cur_pos[2] - self.prev_pos[2] < -0.001:
+            print("b")
             reward = -1
         self.prev_pos = cur_pos   # update prev pos
 
---
+++ Software/vrep_tests/Hexapod/main.py
@@ -0,0 +1,142 @@
+import os
+import sys
+from os.path import abspath, dirname
+sys.path.append(os.path.abspath('..'))
+
+import sim
+# simRemoteApi.start(19999)
+import pickle
+import datetime
+import time
+import numpy as np
+
+import torch
+
+from Hexapod.dqn_agent import Agent
+from Hexapod.hexapod_env import HexapodEnv
+
+
+def test(agent, env, n_episodes=2, max_t=5000):
+    
+    scores = []                        # list containing scores from each episode
+    for i_episode in range(1, n_episodes+1):
+        state = env.reset()
+        time.sleep(0.5)
+        score = 0
+        for _ in range(max_t):
+            action = agent.act(state, eps=0)
+            print(action)
+            next_state, reward, done, _ = env.step(action)
+            state = next_state
+            score += reward
+            if done:
+                time.sleep(0.5)
+                break 
+        scores.append(score)              # save most recent score
+        print('\rEpisode {}\tScore: {:.2f}'.format(i_episode, score))
+    print('--Mean Score {:.2f}--: '.format(np.mean(scores)))
+    return scores
+
+
+
+def print_info(info):
+    print('-'*5,'\t', info, '\t', '-'*5)
+
+
+def open_scene():
+    # robot = int(input("Choose Robot to test:\n1.Poppy Ergo (Hand Robot)\n2.Hexapod\n"))
+    
+        
+    # if robot == 1:  # Poppy Ergo
+    #     pass
+    # elif robot == 2:
+    #     print("Hexapod not implemented")
+    #     quit()
+    # else:
+    #     print("Choose between 1 and 2 only")
+    #     quit()
+    
+    broken_links = int(input("Choose Configuration:\n1.All joints working" \
+                                                    "\n2.Faults in Joint 3" \
+                                                    "\n3.Faults in Joint 2 and 4"
+                                                    "\n4.Faults in Joint 1, 3 and 5"\
+                                                    "\n5.Faults in Joints 1, 2 and 4\n"))
+
+        
+    if broken_links == 1:
+        model_dir = "All_Joints"
+        faulty_joints = -1
+    elif broken_links == 2:
+        model_dir = "Joints_3"
+        faulty_joints = [3] 
+    elif broken_links == 3:
+        model_dir = "Joints_2_4"
+        faulty_joints = [2, 4]
+    elif broken_links == 4:
+        model_dir = "Joints_1_3_5"
+        faulty_joints = [1,3,5] 
+    elif broken_links == 5:
+        model_dir = "Joints_1_2_4"
+        faulty_joints = [1,2,4] 
+    else:
+        print("Choose between given options only")
+        quit()
+
+    model_path = os.path.join("Training_Files", model_dir, "Training_0", "target_network_50")
+    if not os.path.exists(model_path):
+        print("Agent not trained for chose configuration")
+        quit()
+    return faulty_joints, model_path
+
+
+def conect_and_load(port, ip):
+    clientID = sim.simxStart(ip,port,True,True,5000,5) # Connect to CoppeliaSim
+    scene_path = os.path.join('vrep_scene', 'hexapod_scene.ttt')
+    print('-'*5, 'Scene path:', scene_path, '-'*5)
+    sim.simxLoadScene(clientID, scene_path, 0xFF, sim.simx_opmode_blocking)
+    return clientID
+
+
+if __name__ == "__main__":
+    
+    print_info('Program started')
+    sim.simxFinish(-1) # just in case, close all opened connections
+    
+    faulty_joints, model_path = open_scene()
+
+    clientID=conect_and_load(19997, '127.0.0.1')
+    
+    print('-'*10, 'Starting the simulation', '-'*10)
+    sim.simxStartSimulation(clientID, sim.simx_opmode_oneshot)
+    time.sleep(1)
+
+
+    if clientID!=-1:
+        print ('Connected to remote API server')
+        time.sleep(2)
+
+        env = HexapodEnv(clientID, faulty_joints)
+        
+        with open(os.path.join(os.path.dirname(model_path), 'params.pickle'), 'rb') as f: 
+            params = pickle.load(f)
+        
+        # params['num_layers'] = 3
+        # params['hidden_size'] = [256, 128, 64]
+            
+        agent = Agent(env.state_size, env.action_size, params['num_layers'],
+                                        params['hidden_size'], params['seed'])
+
+        # load agent q-network
+        agent.load(model_path)
+        
+        # test
+        test(agent, env, n_episodes=1)
+         
+        # stopping the simulation
+        sim.simxStopSimulation(clientID, sim.simx_opmode_oneshot)
+        print('Stopped the simulation')
+        time.sleep(1)
+        sim.simxFinish(clientID)
+        print('Program finished')
+
+        
